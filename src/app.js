b999adde1383e835478631da7c0afdb679140ca3
Chris Bruford <chrisbruford@gmail.com>
2018-09-23 21:26:01 +0100

    feat: add view

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 import './main.scss';
+import { Gallery } from './scripts/gallery';
+import { TAX_RATE } from './scripts/globals'
+
+
+const cartTarget = document.querySelector('#cart-preview');
+const cartStatus = document.querySelector('#cart-status')
+const sortBy = document.querySelector('#sortBy');
+const ascending = document.querySelector('#ascending');
+const galleryTarget = document.querySelector('#gallery-inner');
+
+const gallery = new Gallery(galleryTarget, cartTarget, cartStatus, TAX_RATE);
+
+sortBy.addEventListener('change',evt=>{
+    let sortBy = evt.target.value;
+    let isAscending = ascending.value === 'true';
+    gallery.populateGallery(gallery.sortProducts(Array.from(gallery.products.values()), sortBy, isAscending));
+});
+
+ascending.addEventListener('change',evt=>{
+    let isAscending = evt.target.value === 'true';
+    let sorted = gallery.sortProducts(Array.from(gallery.products.values()), sortBy.value, isAscending);
+    gallery.populateGallery(sorted, sortBy.value, isAscending);
+})
